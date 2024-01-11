3d3c45490372e08e11f32ad5e897107b2f0f5eea
Adan Cortes Medina <adan.cortes.medina@gmail.com>
2024-01-10 19:39:26 -0600

    [FIX] Add project directory to images location

--- pull.sh
+++ pull.sh
@@ -2,17 +2,19 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd -P)
 
 function update {
+  PROJECT_NAME=$(basename $PORTFOLIO_DIR)
   mkdir -p "$PORTFOLIO_DIR/docs/img"
   README_FILE="$PROJECT_DIR/docs/README.rst"
   cp "$README_FILE" "$PORTFOLIO_DIR/docs"
   cd "$PROJECT_DIR/docs" && egrep "image::" "$README_FILE" | grep -o "img/.*" | xargs -I{} cp {} "$PORTFOLIO_DIR/docs/img"
+  sed -i~ -e "s|.. image:: /docs|.. image:: /$PROJECT_NAME/docs|" "$SCRIPT_DIR/$PROJECT_NAME/docs/README.rst"
 }
 
 PROJECT_DIR="$HOME/Office/cocinadelgallo"
-PORTFOLIO_DIR="$HOME/Office/ants/portfolio/cocinadelgallo"
+PORTFOLIO_DIR="$SCRIPT_DIR/portfolio/cocinadelgallo"
 update 
 
 PROJECT_DIR="$HOME/Office/legar/16desconton"
-PORTFOLIO_DIR="$HOME/Office/ants/portfolio/16desconton"
+PORTFOLIO_DIR="$SCRIPT_DIR/portfolio/16desconton"
 update 
 
